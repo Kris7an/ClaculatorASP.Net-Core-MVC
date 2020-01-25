d8a3efb63eb6d60a16a52e45dfd7c3c50c9d0162
Kris <akumaBG@gmail.com>
2020-01-25 16:19:01 +0200

    Add Code

---
+++ Calc/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Calc.Models;
+
+namespace Calc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public string Income()
+        {
+             return "You have successfuly added income :)";
+
+            //return View();
+        }
+
+        public string Expense()
+        {
+            return "You have successfuly added expense :( Baby's going to be hungry and homeless";
+
+            //return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
