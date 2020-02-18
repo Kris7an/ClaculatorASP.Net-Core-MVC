c96c17815766f573004a94066af3fd979f502dfd
Kris <akumaBG@gmail.com>
2020-02-18 17:20:52 +0200

    test

--- Calc/Startup.cs
+++ Calc/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Calc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +23,10 @@ namespace Calc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CalculatorContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
